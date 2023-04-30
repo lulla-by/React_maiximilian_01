5638d34009363988388dda8889127059b2989f7b
lulla-by <thfl3363@hanmail.net>
2023-04-30 21:39:41 +0900

    양식 제출 정리 - form의 onSubmit속성 -이벤트 객체에서preventDefault()가 필요한 이유

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -3,19 +3,19 @@ import "./ExpenseForm.css"
 
 const ExpenseForm = () => {
   // 1. 여러개의 state를 가지는 방법
-  // const [enteredTitle,setEnteredTitle] = useState("");
-  // const [enteredAmount,setEnteredAmount] = useState("");
-  // const [enteredDate,setEnteredDate] = useState("");
+  const [enteredTitle,setEnteredTitle] = useState("");
+  const [enteredAmount,setEnteredAmount] = useState("");
+  const [enteredDate,setEnteredDate] = useState("");
   
   //2. 한개의 state를 가지는 방법 => 값을 객체로 묶기
-  const [unserInput,setUserInput] = useState({
-    enterdTitle:"",
-    enterdAmount:"",
-    enterdDate:""
-  })
+  // const [unserInput,setUserInput] = useState({
+  //   enterdTitle:"",
+  //   enterdAmount:"",
+  //   enterdDate:""
+  // })
 
   const titleChangeHandler = (e) =>{
-    // setEnteredTitle(e.target.value);
+    setEnteredTitle(e.target.value);
 
     //1번
     // setUserInput({
@@ -24,9 +24,9 @@ const ExpenseForm = () => {
     // })
 
     //2번
-    setUserInput((prevState)=>{
-      return {...prevState,enterdTitle:e.target.value}
-    })
+    // setUserInput((prevState)=>{
+    //   return {...prevState,enterdTitle:e.target.value}
+    // })
   }
 
   // React는 상태 업데이트 스케줄을 갖고 잇어서 바로 실행하지 않음
@@ -34,24 +34,29 @@ const ExpenseForm = () => {
   //2번을 사용할 겅우 리액트는 이 안에 잇는 함수에서 이 상태 스냅샷이 가장 최신의 스냅샷이라는 것과 
   // 항상 계획된 상태 업데이트를 염두에 두고 있다는 것을 보장함
 
-  
+
   const amountChangeHandler = (e) =>{
-    // setEnteredAmount(e.target.value);
-    setUserInput({
-      ...unserInput,
-      enterdAmount:e.target.value
-    })
+    setEnteredAmount(e.target.value);
   }
   const dateChangeHandler = (e) =>{
-    // setEnteredDate(e.target.value);
-    setUserInput({
-      ...unserInput,
-      enterdDate:e.target.value
-    })
+    setEnteredDate(e.target.value);
+  }
+
+  const submitHandler=(e)=>{
+    //버튼을 클릭하면 페이지가 리로드
+    // => 브라우저는 폼이 제출될때마다 이 웹페이즈를 호스팅하고 있는 서버에 요청을 보내기 때문
+    // 이러한 기본동작을 막는것이 preventDefault()
+    e.preventDefault();
+    const expenseData = {
+      title:enteredTitle,
+      amount: enteredAmount,
+      date:new Date(enteredDate)
+    }
+    console.log(expenseData)
   }
 
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label htmlFor="">Title</label>
@@ -68,6 +73,7 @@ const ExpenseForm = () => {
       </div>
       <div className="new-expense__actions">
         <button type='submit'>Add Expense</button>
+        {/* 버튼의 타입이 서브밋이면 폼대신 클릭될경우 이 전체 폼 요소는 수신할 수 있는 이벤트를 생략 */}
       </div>
     </form>
   )
