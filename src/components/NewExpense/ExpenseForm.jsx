dd9ecdee57a2229e93205b0cda1666ff33ccc4d3
lulla-by <thfl3363@hanmail.net>
2023-04-30 21:09:49 +0900

    하나 이상의 상태를 다루는 방법2 - 하나의 state의 값을 객체로 묶기 - 가장 최신의 스냅샷을 사용하는 함수

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -2,19 +2,54 @@ import React, { useState } from 'react';
 import "./ExpenseForm.css"
 
 const ExpenseForm = () => {
-  const [enteredTitle,setEnteredTitle] = useState("");
-  const [enteredAmount,setEnteredAmount] = useState("");
-  const [enteredDate,setEnteredDate] = useState("");
+  // 1. 여러개의 state를 가지는 방법
+  // const [enteredTitle,setEnteredTitle] = useState("");
+  // const [enteredAmount,setEnteredAmount] = useState("");
+  // const [enteredDate,setEnteredDate] = useState("");
+  
+  //2. 한개의 state를 가지는 방법 => 값을 객체로 묶기
+  const [unserInput,setUserInput] = useState({
+    enterdTitle:"",
+    enterdAmount:"",
+    enterdDate:""
+  })
 
   const titleChangeHandler = (e) =>{
-    setEnteredTitle(e.target.value);
+    // setEnteredTitle(e.target.value);
+
+    //1번
+    // setUserInput({
+    //   ...unserInput,
+    //   enterdTitle:e.target.value
+    // })
+
+    //2번
+    setUserInput((prevState)=>{
+      return {...prevState,enterdTitle:e.target.value}
+    })
   }
+
+  // React는 상태 업데이트 스케줄을 갖고 잇어서 바로 실행하지 않음
+  // 동시에 수많은 상태 업데이트를 계획한다면 1번에서는 오래되거나 잘못된 상태 스냅샷에 의존할 수 있음
+  //2번을 사용할 겅우 리액트는 이 안에 잇는 함수에서 이 상태 스냅샷이 가장 최신의 스냅샷이라는 것과 
+  // 항상 계획된 상태 업데이트를 염두에 두고 있다는 것을 보장함
+
+  
   const amountChangeHandler = (e) =>{
-    setEnteredAmount(e.target.value);
+    // setEnteredAmount(e.target.value);
+    setUserInput({
+      ...unserInput,
+      enterdAmount:e.target.value
+    })
   }
   const dateChangeHandler = (e) =>{
-    setEnteredDate(e.target.value);
+    // setEnteredDate(e.target.value);
+    setUserInput({
+      ...unserInput,
+      enterdDate:e.target.value
+    })
   }
+
   return (
     <form>
       <div className="new-expense__controls">
