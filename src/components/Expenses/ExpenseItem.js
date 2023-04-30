d33bba4764e5f9341ad723eb5feb9ce1f0cdaa1b
lulla-by <thfl3363@hanmail.net>
2023-04-30 17:42:56 +0900

    클릭으로 타이틀을 변경하고 싶을때 - 변수의 변경만으로는 트리거를 작동시키지 못해서 변경이 안된다.

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,16 +4,21 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+  //버튼을 클릭했을때 타이틀을 바꾸고 싶을 경우
+  let title = props.title;
+  // 컴포넌트 함수에 변수를 갖고 있고 변수가 변경되었다고(clickHandler()) 해도 리액트는 무시
+  // expenseItem이라는 컴포넌트는 초기에 한번 렌더링되고 다시 호출되지 않을 것
+  // => 왜냐하면 클릭하거나 변수가 변경되었을때 이 컴포넌트 함수를 다시 실행하라고 트리거하지 않을것이기 때문
   const clickHandler = ()=>{
-    console.log("hit")
+    title =  "updated!"
   }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
-      </div>
+      </div> 
       <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
