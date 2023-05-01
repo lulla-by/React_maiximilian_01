37aecc4b946655ee47edcf1c2b84323a8b1e8113
lulla-by <thfl3363@hanmail.net>
2023-05-01 14:39:22 +0900

    데이터의 동적 리스트렌더링 - map()사용

--- src/components/Expenses/Expense.jsx
+++ src/components/Expenses/Expense.jsx
@@ -6,37 +6,24 @@ import ExpensesFilter from "./ExpensesFilter";
 import { useState } from "react";
 
 const Expense = (props) => {
-
-  const [filteredYear,setFilteredYear] = useState("2020")
-  const filteredDate = year => {
-    setFilteredYear(year)
-  }
+  const [filteredYear, setFilteredYear] = useState("2020");
+  const filteredDate = (year) => {
+    setFilteredYear(year);
+  };
   return (
     <div>
-    <ExpensesFilter selected={filteredYear} onSaveDate = {filteredDate}/>
-    <Card className="expenses">
-      <ExpenseItem
-        title={props.expenses[0].title}
-        date={props.expenses[0].date}
-        amount={props.expenses[0].amount}
-        
-      />
-      <ExpenseItem
-        title={props.expenses[1].title}
-        date={props.expenses[1].date}
-        amount={props.expenses[1].amount}
-      />
-      <ExpenseItem
-        title={props.expenses[2].title}
-        date={props.expenses[2].date}
-        amount={props.expenses[2].amount}
-      />
-      <ExpenseItem
-        title={props.expenses[3].title}
-        date={props.expenses[3].date}
-        amount={props.expenses[3].amount}
-      />
-    </Card>
+      <Card className="expenses">
+        <ExpensesFilter selected={filteredYear} onSaveDate={filteredDate} />
+        {/* map()을 사용한 동적 렌더링 */}
+        {props.expenses.map(item => 
+          <ExpenseItem
+            key={item.id}
+            title={item.title}
+            date={item.date}
+            amount={item.amount}
+          />
+        )}
+      </Card>
     </div>
   );
 };
