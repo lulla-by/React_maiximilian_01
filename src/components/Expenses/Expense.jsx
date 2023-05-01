ce37ad0a99e4bc285446fbf3d1cc8bba25b0e995
lulla-by <thfl3363@hanmail.net>
2023-05-01 15:26:59 +0900

    조건부렌더링 - 삼항연산자를 사용

--- src/components/Expenses/Expense.jsx
+++ src/components/Expenses/Expense.jsx
@@ -15,15 +15,11 @@ const Expense = (props) => {
     <div>
       <Card className="expenses">
         <ExpensesFilter selected={filteredYear} onSaveDate={filteredDate} />
-        {/* map()을 사용한 동적 렌더링 */}
-        {filteredExpenses.map(item => 
+        {filteredExpenses.length === 0 
+        ? <p>No expenses found.</p>
+        :filteredExpenses.map(item => 
           <ExpenseItem
-          //  새로운 아이템의 추가시 아이템이 어디에 추가되는지 알려줘야 함
-          //  그래서 아이템 목록이 출력되는 곳에서 특별한 props를 추가하여 해당 작업을 실행
-          //  그 특별한 props가 바로 key props
           key={item.id}
-          //  키가 없을 경우에는 에러 발생 
-          //  => warning: Each child in a list should have a unique "key" prop.
             title={item.title}
             date={item.date}
             amount={item.amount}
