48694fc069a3fbd3fad833fdbe4b1c68703d31fc
lulla-by <thfl3363@hanmail.net>
2023-05-01 14:58:58 +0900

    keys이해하기 - key props의 필요성

--- src/components/Expenses/Expense.jsx
+++ src/components/Expenses/Expense.jsx
@@ -17,7 +17,12 @@ const Expense = (props) => {
         {/* map()을 사용한 동적 렌더링 */}
         {props.expenses.map(item => 
           <ExpenseItem
-            // key={item.id}
+          //  새로운 아이템의 추가시 아이템이 어디에 추가되는지 알려줘야 함
+          //  그래서 아이템 목록이 출력되는 곳에서 특별한 props를 추가하여 해당 작업을 실행
+          //  그 특별한 props가 바로 key props
+          key={item.id}
+          //  키가 없을 경우에는 에러 발생 
+          //  => warning: Each child in a list should have a unique "key" prop.
             title={item.title}
             date={item.date}
             amount={item.amount}
